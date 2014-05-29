ae17316fa75221d1b9c416b620eb3ac3566eea58
Nikhil Jain <nikhil.jain@acm.org>
2014-05-28 22:48:24 -0700

    Add read (but incomplete) functionality to PE
    Change-Id: Icc9c88d4c63ced31acd60ec7a347ab19602a85f2

--- src/pe.C
+++ src/pe.C
@@ -1,49 +1,56 @@
 #include "pe.h"
 
+void PE::execute() {
+  if(++gvt_cnt > gvt_freq) {
+    GVT_begin();
+    gvt_cnt = 0;
+    return;
+  }
+  for(int events = 0; events < batchSize(); events++) {
+    if(!schedule_nextLP())  break;
+  }
+  thisProxy[CkMyPe()].execute();
+}
+
+/* For now, in the synchronous version, invoke completion detection that leads
+ * to a global reduction to find GVT - these may be merged later. The
+ * target of the reduction should be the GVT_end function.
+ */
 void PE::GVT_begin() {
-  /* For now, in the synchronous version,
-   * invoke completion detection that also
-   * performs global reduction to find GVT -
-   * this merging need some thinking. The
-   * target of this operation should be the
-   * GVT_end function.
-   */
+  if(!CkMyPe()) {
+    CkStartQD(CkCallback(CkIndex_PE::GVT_contribute(), thisProxy));
+  }
 }
 
-void PE::update_nextEvents(LP_Entry) {
-  /* Simple inline function to update the
-   * time entry for a given LP in the
-   * queue from which the PE picks the
-   * next event to be executed. Need to
-   * pin point a way to uniquely and
-   * quickly identify the LP in the queue.
-   */
+void PE::GVT_contribute() {
+  Time minTime = getMinTime(); /* TODO: Add min time function */
+  contribute(sizeof(Time), &minTime,CkReduction::min_double, CkCallback(CkReductionTarget(PE,GVT_end),thisProxy));
 }
 
-void PE::update_oldestEvents(LP_Entry) {
-  /* Simple inline function to update the
-   * time entry for a given LP in the
-   * queue which is used by the PE to
-   * determine the LPs on which fossil
-   * collection is needed.
-   */
+void PE::GVT_end(Time newGVT) {
+  gvt = newGVT;
+  collect_fossils();
 }
 
+/* Go over the oldest events queue and call fossil collection on the LPs with
+ * entries less than equal to the new GVT. This may in turn lead to several updates
+ * on this queue for LP's time stamps.
+ */
 void PE::collect_fossils() {
-  /* Go over the oldest events queue and
-   * call fossil collection on the LPs with
-   * entries less than equal to the new GVT.
-   * This may in turn lead to several updates
-   * on this queue for LP's time stamps.
-   */
+  /*TODO Missing functionality in the queue to read the top */
+  LPToken *min = oldestEvents.top();
+  while((min != NULL) && (min->ts < gvt)) {
+    min->lp->fossil_me(gvt);
+  }
 }
 
-void PE::schedule_nextLP() {
-  /* Using the nextEvents queue, execute events
-   * on the LPs in chronological order. Eric noted
-   * that we may be able to optimize here by passing
-   * the next time to the LP which can continue
-   * executing its events till the next time (instead of
-   * only executing one event and returning the control).
-   */
+/* Using the nextEvents queue, execute events
+ * on the LPs in chronological order. Eric noted
+ * that we may be able to optimize here by passing
+ * the next time to the LP which can continue
+ * executing its events till the next time (instead of
+ * only executing one event and returning the control).
+ */
+int PE::schedule_nextLP() {
+
 }
