abedae5d9768202938c8a97b9d39c1979fb47494
Eric Mikida <mikida2@illinois.edu>
2014-07-21 16:02:29 -0500

    Adds more functionality to define_lps

--- src/ross_setup.cpp
+++ src/ross_setup.cpp
@@ -1,5 +1,8 @@
 #include "ross_setup.h"
 
+// Readonly array of lp chares.
+extern CProxy_LP lps;
+
 // TODO: This needs a lot of work
 void tw_init(int* argc, char*** argv) {
   /** Add all of the command line options before parsing them **/
@@ -17,10 +20,30 @@ void tw_init(int* argc, char*** argv) {
 // to know the total number of LPs so we can create a chare array of that size.
 void tw_define_lps(tw_lpid nlp, size_t msg_sz, tw_seed* seed) {
   // TODO: This should probably be in a group chare for config variables
+  // TODO: What will this variable mean in the new ROSS? Right now it is lps on
+  // on this PE which makes no sense. It should be total lps.
   g_tw_nlp = nlp;
 
-  // First we need to figure out the number of KPs (LP Chares)
+  // TODO: Nikhil is working on the memory management portion
+#ifdef ROSS_MEMORY
+  g_tw_memory_sz = sizeof(tw_memory);
+#endif
 
-  // Then we need to create the LP chare array, passing it a mapping so it knows
-  // how to map LPs to itself
+  g_tw_msg_sz = msg_sz;
+  g_tw_rng_seed = seed;
+
+  // TODO: Not implemented yet.
+  /* early_sanity_check(); */
+
+  // Only one processor should create the chare array
+  if (tw_ismaster()) {
+    // First we need to figure out the number of KPs (LP Chares)
+    unsigned num_chares = nlp / g_lps_per_chare;
+
+    // Create the lp chare array and store it in the readonly
+    // TODO: We will eventually pass in a mapping function to the chare array
+    // so it can properly determine which global ids it has.
+    // The constructor also initializes the rng for each lp.
+    lps = CProxy_LP::ckNew(num_chares);
+  }
 }
