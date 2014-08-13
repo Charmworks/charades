c75daf7bfbd1a1b2f174cfbdc51a4f1a1eace096
Nikhil Jain <nikhil.jain@acm.org>
2014-08-13 12:57:43 -0700

    Align ross_util
    Change-Id: Ice3078d10d1f2e41ff03f12cd870b7a9e5568fcb

--- src/ross_util/ross_util.cpp
+++ src/ross_util/ross_util.cpp
@@ -13,83 +13,83 @@
  * We can'd do that of course so we store the message in a buffer until GVT.
  */
 int tw_output(tw_lp *lp, const char *fmt, ...) {
-    int ret = 0;
-    va_list ap;
-    tw_event *cev;
-    tw_out *temp;
+  int ret = 0;
+  va_list ap;
+  tw_event *cev;
+  tw_out *temp;
 
-    if (g_tw_synchronization_protocol != OPTIMISTIC) {
-        va_start(ap, fmt);
-        vfprintf(stdout, fmt, ap);
-        va_end(ap);
-        return 0;
-    }
-
-    // TODO: how do new tw_outs get created?
-    tw_out out;
-
-    cev = lp->pe->cur_event;
-
-    if (cev->out_msgs == 0) {
-        cev->out_msgs = out;
-    }
-    else {
-        // Attach it to the end
-        temp = cev->out_msgs;
-
-        while (temp->next != 0) {
-            temp = temp->next;
-        }
-        temp->next = out;
-    }
-
+  if (g_tw_synchronization_protocol != OPTIMISTIC) {
     va_start(ap, fmt);
-    ret = vsnprintf(out->message, sizeof(out->message), fmt, ap);
+    vfprintf(stdout, fmt, ap);
     va_end(ap);
-    if (ret >= 0 && ret < sizeof(out->message)) {
-        // Should be successful
-    }
-    else {
-        tw_printf(TW_LOC, "Message may be too large?");
-    }
+    return 0;
+  }
 
-    return ret;
+  // TODO: how do new tw_outs get created?
+  tw_out out;
+
+  cev = lp->pe->cur_event;
+
+  if (cev->out_msgs == 0) {
+    cev->out_msgs = out;
+  }
+  else {
+    // Attach it to the end
+    temp = cev->out_msgs;
+
+    while (temp->next != 0) {
+      temp = temp->next;
+    }
+    temp->next = out;
+  }
+
+  va_start(ap, fmt);
+  ret = vsnprintf(out->message, sizeof(out->message), fmt, ap);
+  va_end(ap);
+  if (ret >= 0 && ret < sizeof(out->message)) {
+    // Should be successful
+  }
+  else {
+    tw_printf(TW_LOC, "Message may be too large?");
+  }
+
+  return ret;
 }
 
-void
+  void
 tw_printf(const char *file, int line, const char *fmt, ...)
 {
-	va_list	ap;
+  va_list	ap;
 
-	va_start(ap, fmt);
-	fprintf(stdout, "%s:%i: ", file, line);
-	vfprintf(stdout, fmt, ap);
-	fprintf(stdout, "\n");
-	fflush(stdout);
-	va_end(ap);
+  va_start(ap, fmt);
+  fprintf(stdout, "%s:%i: ", file, line);
+  vfprintf(stdout, fmt, ap);
+  fprintf(stdout, "\n");
+  fflush(stdout);
+  va_end(ap);
 }
 
-void
+  void
 tw_error(const char *file, int line, const char *fmt, ...)
 {
-	va_list	ap;
+  va_list	ap;
 
-	va_start(ap, fmt);
-	fprintf(stdout, "node: %ld: error: %s:%i: ", g_tw_mynode, file, line);
-	vfprintf(stdout, fmt, ap);
-	fprintf(stdout, "\n");
-	fflush(stdout);
-	fflush(stdout);
-	va_end(ap);
+  va_start(ap, fmt);
+  fprintf(stdout, "node: %ld: error: %s:%i: ", g_tw_mynode, file, line);
+  vfprintf(stdout, fmt, ap);
+  fprintf(stdout, "\n");
+  fflush(stdout);
+  fflush(stdout);
+  va_end(ap);
 
-	tw_net_abort();
+  tw_net_abort();
 }
 
 struct mem_pool
 {
-	struct mem_pool *next_pool;
-	char *next_free;
-	char *end_free;
+  struct mem_pool *next_pool;
+  char *next_free;
+  char *end_free;
 }__attribute__((aligned(8)));
 
 static struct mem_pool *main_pool;
@@ -103,101 +103,101 @@ static void* my_malloc(size_t len);
 
 void
 tw_calloc_stats(
-	size_t *bytes_alloc,
-	size_t *bytes_wasted)
+    size_t *bytes_alloc,
+    size_t *bytes_wasted)
 {
-	struct mem_pool *p;
+  struct mem_pool *p;
 
-	*bytes_alloc = total_allocated;
-	*bytes_wasted = malloc_calls * (sizeof(void*) + sizeof(size_t));
+  *bytes_alloc = total_allocated;
+  *bytes_wasted = malloc_calls * (sizeof(void*) + sizeof(size_t));
 
-	for (p = main_pool; p; p = p->next_pool)
-		*bytes_wasted += p->end_free - p->next_free;
+  for (p = main_pool; p; p = p->next_pool)
+    *bytes_wasted += p->end_free - p->next_free;
 }
 
-static void*
+  static void*
 pool_alloc(size_t len)
 {
-	struct mem_pool *p;
-	void *r;
+  struct mem_pool *p;
+  void *r;
 
-	for (p = main_pool; p; p = p->next_pool)
-		if ((p->end_free - p->next_free >= len))
-			break;
+  for (p = main_pool; p; p = p->next_pool)
+    if ((p->end_free - p->next_free >= len))
+      break;
 
-	if (!p) {
-		if (len >= pool_size) {
-			r = my_malloc(len);
-			goto ret;
-		}
+  if (!p) {
+    if (len >= pool_size) {
+      r = my_malloc(len);
+      goto ret;
+    }
 
-		p = (struct mem_pool *) my_malloc(pool_size + 32);
-		if (!p) {
-			r = NULL;
-			goto ret;
-		}
+    p = (struct mem_pool *) my_malloc(pool_size + 32);
+    if (!p) {
+      r = NULL;
+      goto ret;
+    }
 
-		p->next_pool = main_pool;
-		//p->next_free = (char*)(p + 1);
-                p->next_free = (char *)((size_t)32 + (size_t)p);
-		if( 7 & (size_t)(p->next_free) )
-		    printf("pool_alloc: WARNING found pool start address (%p) NOT 8 byte aligned\n", p->next_free);
-		p->end_free = p->next_free + pool_size;
-		main_pool = p;
-	}
+    p->next_pool = main_pool;
+    //p->next_free = (char*)(p + 1);
+    p->next_free = (char *)((size_t)32 + (size_t)p);
+    if( 7 & (size_t)(p->next_free) )
+      printf("pool_alloc: WARNING found pool start address (%p) NOT 8 byte aligned\n", p->next_free);
+    p->end_free = p->next_free + pool_size;
+    main_pool = p;
+  }
 
-	r = p->next_free;
-	p->next_free += len;
+  r = p->next_free;
+  p->next_free += len;
 
-	if( 7 & (size_t)r || 7 & (size_t)(p->next_free) )
-	    printf("pool_alloc: WARNING found return ptr (%p) or next_free (%p) NOT 8 bytes aligned\n", r, p->next_free );
+  if( 7 & (size_t)r || 7 & (size_t)(p->next_free) )
+    printf("pool_alloc: WARNING found return ptr (%p) or next_free (%p) NOT 8 bytes aligned\n", r, p->next_free );
 
 ret:
-	if (r)
-		total_allocated += len;
-	return r;
+  if (r)
+    total_allocated += len;
+  return r;
 }
 
 void*
 tw_calloc(
-	const char *file,
-	int line,
-	const char *for_who,
-	size_t e_sz,
-	size_t n)
+    const char *file,
+    int line,
+    const char *for_who,
+    size_t e_sz,
+    size_t n)
 {
-	void *r;
+  void *r;
 
-	if(e_sz & (pool_align - 1))
-	{
-	    e_sz += pool_align - (e_sz & (pool_align - 1));
-	    // printf("%s:%d:%s: realigned size to %d \n", file, line, for_who, e_sz );
-	}
+  if(e_sz & (pool_align - 1))
+  {
+    e_sz += pool_align - (e_sz & (pool_align - 1));
+    // printf("%s:%d:%s: realigned size to %d \n", file, line, for_who, e_sz );
+  }
 
-	e_sz *= n;
-	if (!e_sz)
-		return NULL;
+  e_sz *= n;
+  if (!e_sz)
+    return NULL;
 
-	r = pool_alloc(e_sz);
-	if (!r)
-		tw_error(
-			file, line,
-			"Cannot allocate %lu bytes for %u %s"
-			" (need total of %lu KiB)",
-			(unsigned long)e_sz,
-			n,
-			for_who,
-			(unsigned long)((total_allocated + e_sz) / 1024));
-	memset(r, 0, e_sz);
-	return r;
+  r = pool_alloc(e_sz);
+  if (!r)
+    tw_error(
+        file, line,
+        "Cannot allocate %lu bytes for %u %s"
+        " (need total of %lu KiB)",
+        (unsigned long)e_sz,
+        n,
+        for_who,
+        (unsigned long)((total_allocated + e_sz) / 1024));
+  memset(r, 0, e_sz);
+  return r;
 }
 
 #undef malloc
-static void*
+  static void*
 my_malloc(size_t len)
 {
-	malloc_calls++;
-	return malloc(len);
+  malloc_calls++;
+  return malloc(len);
 }
 
 #undef realloc
