0973c61bbb59f307f4c5a769aa6ee3350dd770d2
Eric Mikida <mikida2@illinois.edu>
2014-10-11 22:34:28 -0500

    Adds missing charm_api.h

---
+++ src/charm/charm_api.h
@@ -0,0 +1,17 @@
+// Defines a C-Style API that is exposed to the ross backend
+
+// PE API
+
+// LP API
+void create_lps();
+void init_lps();
+void set_current_event(tw_lp*, tw_event*);
+tw_event* current_event(tw_lp*);
+tw_stime tw_now(tw_lp*);
+
+// Event API
+tw_event * charm_allocate_event(int needMsg = 1);
+void charm_free_event(tw_event * e);
+void charm_event_cancel(tw_event * e);
+void charm_event_send(unsigned, tw_event * e);
+void charm_anti_send(unsigned, tw_event * e);
