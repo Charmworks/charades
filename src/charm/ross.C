8bf5c62094ce70fe3bd797095cd3245c1cc5786e
Nikhil Jain <nikhil@illinois.edu>
2014-10-13 02:37:21 +0000

    Interface has been unified, remove code duplication
    Change-Id: Iebac74dd0e148bd411ed31cd057392ca95bea0b8

--- src/charm/ross.C
+++ src/charm/ross.C
@@ -6,12 +6,7 @@ extern CProxy_PE pes;
 // Function that starts the charm library and results in the creation of the
 // PE group chares.
 void charm_init(int argc, char** argv) {
-/*TODO: change Charm interface */
-#if CMK_CONVERSE_MPI
   CharmInit(argc, argv);
-#else
-  CharmInit(argc, argv);
-#endif
 }
 
 Initialize::Initialize(CkArgMsg *m) {
