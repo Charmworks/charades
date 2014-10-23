2fc806da54d9eac01f4e6fc666ddcbe39f333062
Elsa Gonsiorowski <gonsie@me.com>
2014-10-23 16:33:29 -0400

    made the statistic print function non-static

--- src/ross_util/ross_util.h
+++ src/ross_util/ross_util.h
@@ -10,6 +10,11 @@
 //static tw_stime percent_complete = 0.0;
 //inline void gvt_print(Time gvt);
 
+void show_lld(const char *name, tw_stat v);
+void show_2f(const char *name, double v);
+void show_1f(const char *name, double v);
+void show_4f(const char *name, double v);
+
 void tw_stats_old(Statistics *me);
 
 int tw_output (tw_lp *lp, const char *fmt, ...);
