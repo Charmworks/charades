9d398157d881deb353d65c86438bbf0ca60eee37
Eric Mikida <mikida2@illinois.edu>
2015-03-24 20:58:22 -0500

    Experiment with fixing an event buffer bug

--- src/collections/event_buffer.h
+++ src/collections/event_buffer.h
@@ -25,22 +25,24 @@ class EventBuffer {
         max_events(max), msg_size(sz),
         stack_pointer(max), remote_stack_pointer(max*0.1) {
       abort_event = new Event;
-      abort_event->eventMsg = new (msg_size, 32) RemoteEvent;
+      abort_event->eventMsg = new (msg_size) RemoteEvent;
       abort_event->userData = abort_event->eventMsg->userData;
 
       int err = posix_memalign((void **)&buffer, 64, max*sizeof(Event*));
       err = posix_memalign((void **)&remote_buffer, 64, max*sizeof(RemoteEvent*));
 
-      int buf_size = 16*8;
-      char *tempbuf;
-      err = posix_memalign((void **)&tempbuf, 64, stack_pointer*buf_size);
-      if(sizeof(Event) > buf_size) {
-        printf("Buffers messed up, fix me!!\n");
-      }
+      //buffer = new Event*[max];
+      Event* temp_buf = new Event[max];
 
-      for (int i = 0; i < stack_pointer; i++) {
-        buffer[i] = (Event*)tempbuf;
-        tempbuf += buf_size;
+      //int buf_size = 16*8;
+      //char *tempbuf;
+      //err = posix_memalign((void **)&tempbuf, 64, stack_pointer*buf_size);
+      //if(sizeof(Event) > buf_size) {
+      //  printf("Buffers messed up, fix me!!\n");
+      //}
+
+      for (int i = 0; i < max; i++) {
+        buffer[i] = &temp_buf[i];
       }
       for (int i = 0; i < remote_stack_pointer; i++) {
         remote_buffer[i] = new (msg_size) RemoteEvent;
