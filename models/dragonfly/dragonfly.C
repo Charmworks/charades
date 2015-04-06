8305046dc79d00ec1aaedcd957398779451c6168
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2015-04-06 14:50:20 -0400

    get_*_rem takes chare index

--- models/dragonfly/dragonfly.C
+++ models/dragonfly/dragonfly.C
@@ -31,27 +31,27 @@ int nlp_mpi_procs_per_chare (unsigned chare_index) {
 
 FILE * dragonfly_event_log=NULL;
 int
-get_terminal_rem()
+get_terminal_rem(unsigned chare_index)
 {
-   if(terminal_rem > 0 && terminal_rem <= g_tw_mynode)
+   if(terminal_rem > 0 && terminal_rem <= chare_index)
     return terminal_rem;
 
    return 0;
 }
 
 int
-get_router_rem()
+get_router_rem(unsigned chare_index)
 {
-  if(router_rem > 0 && router_rem <= g_tw_mynode)
+  if(router_rem > 0 && router_rem <= chare_index)
    return router_rem;
 
   return 0;
 }
 
 int
-get_inv_router_rem()
+get_inv_router_rem(unsigned chare_index)
 {
-   if(router_rem > 0 && router_rem >= g_tw_mynode)
+   if(router_rem > 0 && router_rem >= chare_index)
 	return router_rem;
 
   return 0;
@@ -1576,12 +1576,14 @@ tw_lpid dragonfly_mapping_to_lp(tw_lpid lpid)
   if (g_tw_mynode < terminal_rem)
     nlp_mpi_procs_per_pe++;
 
+  unsigned chare_index = mapping(lpid);
+
   if(lpid < total_routers)
-      index = lpid - g_tw_mynode * nlp_router_per_pe - get_router_rem();
+      index = lpid - g_tw_mynode * nlp_router_per_pe - get_router_rem(chare_index);
   else if(lpid >= total_routers && lpid < total_routers+total_terminals)
-      index = nlp_router_per_pe + (lpid - g_tw_mynode * nlp_terminal_per_pe - get_terminal_rem() - total_routers);
+      index = nlp_router_per_pe + (lpid - g_tw_mynode * nlp_terminal_per_pe - get_terminal_rem(chare_index) - total_routers);
   else if (lpid >= total_routers + total_terminals && lpid < total_routers + total_terminals + total_mpi_procs)
-	    index = nlp_router_per_pe + nlp_terminal_per_pe + (lpid - g_tw_mynode * nlp_mpi_procs_per_pe - get_terminal_rem() - total_routers - total_terminals);
+	    index = nlp_router_per_pe + nlp_terminal_per_pe + (lpid - g_tw_mynode * nlp_mpi_procs_per_pe - get_terminal_rem(chare_index) - total_routers - total_terminals);
   else
       tw_error(TW_LOC, "LPID out of bounds.");
 
@@ -1592,13 +1594,13 @@ tw_lpid dragonfly_mapping (unsigned chare_index, tw_lpid local_id) {
   tw_lpid gid;
   if (local_id < nlp_router_per_chare(chare_index)) {
     // router type
-    gid = g_tw_mynode * nlp_router_per_chare(chare_index) + local_id + get_router_rem();
+    gid = g_tw_mynode * nlp_router_per_chare(chare_index) + local_id + get_router_rem(chare_index);
   } else if (local_id < nlp_router_per_chare(chare_index) + nlp_terminal_per_chare(chare_index)) {
     // terminal type
-    gid = total_routers + g_tw_mynode * nlp_terminal_per_chare(chare_index) + local_id + get_terminal_rem();
+    gid = total_routers + g_tw_mynode * nlp_terminal_per_chare(chare_index) + local_id + get_terminal_rem(chare_index);
   } else if (local_id < nlp_router_per_chare(chare_index) + nlp_terminal_per_chare(chare_index) + nlp_mpi_procs_per_chare(chare_index)) {
     // mpi_proc type
-    gid = total_routers + total_terminals + g_tw_mynode * nlp_mpi_procs_per_chare(chare_index) + local_id + get_terminal_rem();
+    gid = total_routers + total_terminals + g_tw_mynode * nlp_mpi_procs_per_chare(chare_index) + local_id + get_terminal_rem(chare_index);
   } else {
     tw_error(TW_LOC, "Mapping setup ERROR: unrecognized local id");
   }
