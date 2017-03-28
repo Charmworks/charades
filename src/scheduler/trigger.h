336dd35f9ecade81602443138eb2e517a7bb04cc
Eric Mikida <mikida2@illinois.edu>
2017-03-28 15:45:56 -0500

    Cleanup trigger code (temporarily removes LeashTrigger)
    Change-Id: I0e2664fb1a979c0190f69201238c8ca1936697dd

---
+++ src/scheduler/trigger.h
@@ -0,0 +1,46 @@
+#ifndef _TRIGGER_H_
+#define _TRIGGER_H_
+
+#include "typedefs.h"
+#include "scheduler.h"
+#include "gvtmanager.h"
+
+class Trigger {
+  public:
+    virtual void iteration_done() = 0;
+    virtual void reset() = 0;
+    virtual bool ready() const = 0;
+};
+
+class BoundedTrigger : public Trigger {
+  private:
+    std::unique_ptr<Trigger> trigger;
+    unsigned triggers_left;
+  public:
+    BoundedTrigger(Trigger* t, unsigned triggers) : trigger(t), triggers_left(triggers) {}
+    void iteration_done() { trigger->iteration_done(); }
+    void reset() { if (triggers_left > 0) triggers_left--; trigger->reset(); }
+    bool ready() const { return triggers_left > 0 && trigger->ready(); }
+};
+
+class ConstTrigger : public Trigger {
+  private:
+    bool value;
+  public:
+    ConstTrigger(bool v) : value(v) {}
+    void iteration_done() {}
+    void reset() {}
+    bool ready() const { return value; }
+};
+
+class CountTrigger : public Trigger {
+  private:
+    unsigned int iter_cnt, iter_max;
+  public:
+    CountTrigger(unsigned max) : iter_cnt(0), iter_max(max) {}
+    void iteration_done() { iter_cnt++; }
+    void reset() { iter_cnt = 0; }
+    bool ready() const { return iter_cnt == iter_max; }
+};
+
+#endif
