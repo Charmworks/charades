3c2727f761c9e60df36c661da3ba837e53cdbef9
Eric Mikida <mikida2@illinois.edu>
2017-03-28 01:41:37 -0500

    Make a scheduler directory and break out individual schedulers
    Change-Id: I1de17272795cc61d4e82cabb1d035d3520536635

---
+++ src/scheduler/scheduler.C
@@ -0,0 +1,108 @@
+#include "scheduler.h"
+
+#include "charm_functions.h"
+#include "gvtmanager.h"
+#include "lp.h"
+#include "ross.h"
+#include "globals.h"
+#include "ross_random.h"
+#include "avl_tree.h"
+#include "ross_setup.h" // tmp for AVL_NODE_COUNT
+#include "event_buffer.h"
+
+#include <float.h> // Included for DBL_MAX
+
+CProxy_Scheduler scheduler_proxy;
+
+/******************************************************************************/
+/* Scheduler Base Class                                                       */
+/******************************************************************************/
+Scheduler::Scheduler() {
+  scheduler_proxy = thisProxy;
+  globals = new Globals();
+  stats = new Statistics();
+
+  contribute(CkCallback(CkReductionTarget(Scheduler, schedulerReady), thisProxy));
+  thisProxy[CkMyPe()].initialize();
+}
+
+void Scheduler::initialize_rand() {
+  DEBUG_PE("Random number generator initialized\n");
+  rng = tw_rand_init(31, 41);
+}
+
+void Scheduler::initialize_events() {
+  PE_VALUE(event_buffer) = new EventBuffer(g_tw_max_events_buffered,
+                                           g_tw_max_remote_events_buffered,
+                                           g_tw_msg_sz);
+  PE_VALUE(abort_event) = PE_VALUE(event_buffer)->get_abort_event();
+  DEBUG_PE("Created event buffer with %d events and %d msgs of size %d\n",
+      g_tw_max_events_buffered, g_tw_max_remote_events_buffered, g_tw_msg_sz);
+}
+
+void Scheduler::start_simulation() {
+  start_time = CmiWallTimer();
+  scheduler_proxy[CkMyPe()].execute();
+}
+
+void Scheduler::end_simulation() {
+  end_time = CmiWallTimer();
+  stats->total_time = end_time - start_time;
+  contribute(sizeof(Statistics), stats, statsReductionType,
+      CkCallback(CkIndex_Scheduler::finalize(NULL), thisProxy[0]));
+}
+
+void Scheduler::finalize(CkReductionMsg* msg) {
+  Statistics* final_stats = (Statistics*)msg->getData();
+  final_stats->print();
+  CkExit();
+}
+
+/** Return the minimum LP time on this PE */
+// TODO: Keep a "current_time" var per PE and update it when update_next happens
+Time Scheduler::get_min_time() const {
+  return next_lps.top() != NULL ? next_lps.top()->ts : DBL_MAX;
+}
+
+/** Pull the next LP from the queue and have it execute an event */
+bool Scheduler::schedule_next_lp() {
+  LPToken *min = next_lps.top();
+  if(min == NULL) return false;
+  if (lps(min->lp->thisIndex).execute_me()) {
+    PE_STATS(events_executed)++;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/** Needs to be overridden by each base class to define a scheduler iteration */
+void Scheduler::execute() {
+  CkAbort("Need to instantiate a specific Scheduler subclass\n");
+}
+
+void Scheduler::gvt_resume() {}
+
+void Scheduler::gvt_done(Time gvt) {
+  if(gvt >= g_tw_ts_end) {
+    end_simulation();
+  } else {
+    thisProxy[CkMyPe()].execute();
+  }
+}
+
+/******************************************************************************/
+/* Sequential Scheduler                                                       */
+/******************************************************************************/
+SequentialScheduler::SequentialScheduler() {
+  scheduler_name = "Sequential Scheduler";
+}
+
+void SequentialScheduler::execute() {
+  CkAbort("SequentialScheduler not yet implemented\n");
+}
+
+
+#include "conservative.h"
+#include "optimistic.h"
+#include "scheduler.def.h"
--- src/scheduler/scheduler.h
+++ src/scheduler/scheduler.h
@@ -98,12 +98,6 @@ class SequentialScheduler : public CBase_SequentialScheduler {
     void execute();
 };
 
-class ConservativeScheduler : public CBase_ConservativeScheduler {
-  public:
-    ConservativeScheduler();
-    void execute();
-};
-
 // TODO: Change iteration_done to is_ready or something like that
 // TODO: Add gvt_done(gvt) method and maybe one for resume as well...since this
 // is the GVT trigger, signalling it when GVT completes and when iter completes
@@ -142,48 +136,6 @@ class LeashTrigger : public GVTTrigger {
     void gvt_done(Time gvt) { leash_start = gvt; }
     bool is_ready(Time next) const {
       return (leash_start + leash_length <= next);
-    }
-};
-
-class OptimisticScheduler : public CBase_OptimisticScheduler {
-  private:
-    Time min_cancel_time; /**< minumum event time in the cancel queue */
-    // TODO: Don't use this...instead just do what FC does and call the method
-    // on ever LP on PE. This can use the PE level list of LPs. In fact, if we
-    // allow for some sort of visitor pattern we can do the same for cancel
-    // processing, LB control, and FC. Should maybe even be faster because we
-    // won't need to constantly mess with the vector of LPs.
-    vector<LP*> cancel_q; /**< list of LPs with events for cancellation */
-
-    // TODO: Maybe this could be a template instead
-    // TODO: For leash with async red or something like that the GVTMan needs a
-    // leash estimate (probably in just holding local min)
-    CountTrigger trigger;
-
-  public:
-    OptimisticScheduler();
-    void initialize_events();
-    void execute();
-    void gvt_resume();
-    void gvt_done(Time gvt);
-    void collect_fossils(Time gvt);       /**< collect fossils */
-    void process_cancel_q();      /**< process the cancel_q */
-    void add_to_cancel_q(LP*);    /**< add an LP to the cancel_q */
-    void update_min_cancel(Time); /**< update min_cancel_time */
-    Time get_min_time() const;    /**< Override base method to include cancel q*/
-
-    // TODO: What does it mean to remove from cancel queue if it's actually in
-    // the cancel queue?
-    void unregister_lp(LPToken* next_token) {
-      vector<LP*>::iterator it = cancel_q.begin();
-      while (it != cancel_q.end()) {
-        if (*it == next_token->lp) {
-          cancel_q.erase(it);
-          break;
-        }
-        it++;
-      }
-      Scheduler::unregister_lp(next_token);
     }
 };
 
