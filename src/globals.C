4a5432163b74de0258775c42e3ea9cbbdcbb4816
Eric Mikida <mikida2@illinois.edu>
2017-07-07 15:48:57 -0500

    Add some initial Doxygen documentation to globals

--- src/globals.C
+++ src/globals.C
@@ -1,89 +1,129 @@
+/** \file globals.C
+ *  Definitions for global variables and associated functions.
+ *
+ *  \todo Many of these variables really belong to specific modules, and can
+ *  also be made non-global.
+ */
+
 #include "globals.h"
 
 #include "ross_block.h"
 #include "scheduler.h"
 
-numlp_map_f g_numlp_map;  // chare -> numlps on that chare
-init_map_f  g_init_map;   // (chare x lid) -> gid
-type_map_f  g_type_map;   // gid -> type
-local_map_f g_local_map;  // gid -> lid
-chare_map_f g_chare_map;  // gid -> chare
+/** \name LP Configuration
+ */
+///@{
+unsigned g_total_lps;
+unsigned g_num_chares;
+unsigned g_lps_per_chare;
 
+numlp_map_f g_numlp_map;
+init_map_f  g_init_map;
+type_map_f  g_type_map;
+local_map_f g_local_map;
+chare_map_f g_chare_map;
+///@}
+
+/** \name Event Configuration */
+///@{
+tw_stime g_tw_lookahead;
+size_t   g_tw_msg_sz;
+unsigned g_tw_max_events_buffered;
+unsigned g_tw_max_remote_events_buffered;
+///@}
+
+/** \name Scheduler Configuration */
+///@{
 unsigned g_tw_synchronization_protocol;
+unsigned g_tw_mblock;
+tw_stime g_tw_ts_end;
+///@}
+
+/** \name GVT Configuration */
+///@{
 unsigned g_tw_gvt_scheme;
-int g_tw_expected_events;
-tw_stime g_tw_ts_end;       // end time of simulation
-unsigned g_tw_mblock;       // number of events per gvt interval
-unsigned g_tw_gvt_interval; // number of intervals per gvt
-unsigned g_tw_gvt_trigger ; // GVT trigger type
-unsigned g_tw_gvt_phases;   // number of phases of the gvt
-unsigned g_tw_gvt_bucket_size; // size of each bucket for bucketed gvt
-unsigned g_tw_greedy_start; // whether we allow a greedy start or not
-unsigned g_tw_async_reduction; // allow GVT reduction and event exec to overlap
-unsigned g_tw_ldb_interval; // number of intervals to wait before ldb
-unsigned g_tw_ldb_first;    // first interval to call load balancing
-unsigned g_tw_max_ldb;      // max number of times we will load balance
-unsigned g_tw_ldb_metric;    // Metric used to measure LP load
-unsigned g_tw_metric_ts_abs;// Use absolute time or not
-unsigned g_tw_metric_invert;// Whether or not to invert metric
-unsigned g_tw_stat_interval;// number of intervals between stat logging
-tw_stime g_tw_lookahead;    // event lookahead for conservative
-tw_stime g_tw_leash;        // gvt leash for optimistic
-unsigned gvt_print_interval; // determines frequency of progress print outs
+unsigned g_tw_gvt_interval;
+unsigned g_tw_gvt_trigger;
+unsigned g_tw_gvt_phases;
+unsigned g_tw_gvt_bucket_size;
+unsigned g_tw_async_reduction;
+///@}
+
+/** \name LB Configuration */
+///@{
+unsigned g_tw_ldb_first;
+unsigned g_tw_ldb_interval;
+unsigned g_tw_max_ldb;
+unsigned g_tw_ldb_metric;
+unsigned g_tw_metric_ts_abs;
+unsigned g_tw_metric_invert;
+///@}
+
+/** \name RNG Configuration */
+///@{
 tw_seed* g_tw_rng_seed;
 size_t   g_tw_rng_max;
 unsigned g_tw_nRNG_per_lp;
 unsigned g_tw_rng_default;
-unsigned g_num_chares;    // number of chares
-unsigned g_lps_per_chare; // number of LPs per chare (if constant)
-unsigned g_total_lps;     // number of LPs in the simulation
-size_t   g_tw_msg_sz;
-unsigned g_tw_max_events_buffered;
-unsigned g_tw_max_remote_events_buffered;
+///@}
 
-// Function for setting default/initial values
+/** \name Misc Configuration */
+///@{
+int      g_tw_expected_events;
+unsigned gvt_print_interval;
+unsigned g_tw_stat_interval;
+///@}
+
+/**
+ * \todo Can these just be initialized at declaration? Especially if they are
+ * all moved to more appropriate modules.
+ */
 void clear_globals() {
-  g_tw_synchronization_protocol = CONSERVATIVE;
-  g_tw_gvt_scheme    = 1;
-  g_tw_expected_events = -1;
-  g_tw_ts_end        = 100000.0;
-  g_tw_mblock        = 16;
-  g_tw_gvt_interval  = 16;
-  g_tw_gvt_trigger   = 1;
-  g_tw_gvt_phases    = 2;
-  g_tw_gvt_bucket_size    = 8;
-  g_tw_async_reduction = 0;
-  g_tw_ldb_interval  = 0;
-  g_tw_ldb_first     = 0;
-  g_tw_stat_interval = 0;
-  g_tw_max_ldb       = 0;
-  g_tw_ldb_metric    = 0;
-  g_tw_metric_ts_abs = 0;
-  g_tw_metric_invert = 0;
-  g_tw_lookahead     = 0.005;
-  gvt_print_interval = 64;
-
-  g_tw_rng_seed    = NULL;
-  g_tw_rng_max     = 1;
-  g_tw_nRNG_per_lp = 1;
-  g_tw_rng_default = 1;
-
-  g_lps_per_chare  = 1;
-  g_num_chares     = 1;
   g_total_lps      = 1;
-
-  g_tw_msg_sz              = 0;
-  g_tw_max_events_buffered = 1024;
-  g_tw_max_remote_events_buffered = 1024;
+  g_num_chares     = 1;
+  g_lps_per_chare  = 1;
 
   g_numlp_map  = NULL;
   g_init_map   = init_block_map;
   g_type_map   = NULL;
   g_local_map  = local_block_map;
   g_chare_map  = chare_block_map;
+
+  g_tw_lookahead                  = 0.005;
+  g_tw_msg_sz                     = 0;
+  g_tw_max_events_buffered        = 1024;
+  g_tw_max_remote_events_buffered = 1024;
+
+  g_tw_synchronization_protocol = CONSERVATIVE;
+  g_tw_mblock                   = 16;
+  g_tw_ts_end                   = 100000.0;
+
+  g_tw_gvt_scheme       = 1;
+  g_tw_gvt_interval     = 16;
+  g_tw_gvt_trigger      = 1;
+  g_tw_gvt_phases       = 2;
+  g_tw_gvt_bucket_size  = 8;
+  g_tw_async_reduction  = 0;
+
+  g_tw_ldb_first     = 0;
+  g_tw_ldb_interval  = 0;
+  g_tw_max_ldb       = 0;
+  g_tw_ldb_metric    = 0;
+  g_tw_metric_ts_abs = 0;
+  g_tw_metric_invert = 0;
+
+  g_tw_rng_seed    = NULL;
+  g_tw_rng_max     = 1;
+  g_tw_nRNG_per_lp = 1;
+  g_tw_rng_default = 1;
+
+  gvt_print_interval = 64;
+  g_tw_stat_interval = 0;
+  g_tw_expected_events = -1;
 }
 
 Globals* get_globals() {
+  /** Store the pointer in a static variable for faster lookup */
   static Globals* globals = ((Scheduler*)CkLocalBranch(scheduler_id))->globals;
   return globals;
 }
--- src/globals.h
+++ src/globals.h
@@ -1,63 +1,126 @@
+/** \file globals.h
+ *  Declarations for global variables and associated functions.
+ *
+ *  Most declarations in this file are for "constant" global variables that are
+ *  set during startup based on model/simulator configuration and don't ever
+ *  change. Variables that are global per PE, but will change at a PE level are
+ *  wrapped in a small class which will be instantiated once on each PE.
+ */
+
 #ifndef GLOBALS_H_
 #define GLOBALS_H_
 
 #include "typedefs.h"
 
-extern numlp_map_f g_numlp_map;  // chare -> numlps on that chare
-extern init_map_f  g_init_map;   // (chare x lid) -> gid
-extern type_map_f  g_type_map;   // gid -> type
-extern local_map_f g_local_map;  // gid -> lid
-extern chare_map_f g_chare_map;  // gid -> chare
+/** \name LP Configuration
+ */
+///@{
+extern unsigned g_total_lps;     ///< total number of LPs
+extern unsigned g_num_chares;    ///< total number of chares
+extern unsigned g_lps_per_chare; ///< number of LPs per chare (if constant)
 
-extern unsigned g_tw_synchronization_protocol;
-extern unsigned g_tw_gvt_scheme;
-extern int g_tw_expected_events;
-extern tw_stime g_tw_ts_end;       // end time of simulation
-extern unsigned g_tw_mblock;       // number of events per gvt interval
-extern unsigned g_tw_gvt_interval; // number of intervals per gvt
-extern unsigned g_tw_gvt_trigger;  // GVT trigger type
-extern unsigned g_tw_gvt_phases;   // number of phases in the gvt
-extern unsigned g_tw_gvt_bucket_size;   // size of each bucket for bucketed gvt
-extern unsigned g_tw_async_reduction; // allow GVT rdn and event exec overlap
-extern unsigned g_tw_ldb_interval; // number of intervals to wait before ldb
-extern unsigned g_tw_ldb_first;    // first iteration to call load balancing
-extern unsigned g_tw_max_ldb;      // number of intervals to wait before ldb
-extern unsigned g_tw_ldb_metric;   // Metric used to measure LP load
-extern unsigned g_tw_metric_ts_abs;// Use absolute time or not
-extern unsigned g_tw_metric_invert;// Whether or not to invert metric
-extern unsigned g_tw_stat_interval;// number of gvts between logging stats
-extern tw_stime g_tw_lookahead;    // event lookahead for conservative
-extern unsigned gvt_print_interval;// determines frequency of progress print outs
-extern tw_seed* g_tw_rng_seed;
-extern size_t   g_tw_rng_max;
-extern unsigned g_tw_nRNG_per_lp;
-extern unsigned g_tw_rng_default;
-extern unsigned g_num_chares;    // number of chares
-extern unsigned g_lps_per_chare; // number of LPs per chare (if constant)
-extern unsigned g_total_lps;     // number of LPs in the simulation
-extern size_t   g_tw_msg_sz;
-extern unsigned g_tw_max_events_buffered;
-extern unsigned g_tw_max_remote_events_buffered;
+extern numlp_map_f g_numlp_map;  ///< map from chare id to number of lps on that chare
+extern init_map_f  g_init_map;   ///< map from chare id and local lp id to global lp id
+extern type_map_f  g_type_map;   ///< map from global lp id to lp type
+extern local_map_f g_local_map;  ///< map from global lp id to local lp id
+extern chare_map_f g_chare_map;  ///< map from global lp id to chare id
+///@}
+
+/** \name Event Configuration */
+///@{
+extern tw_stime g_tw_lookahead;                   ///< event lookahead (conservative)
+extern size_t   g_tw_msg_sz;                      ///< user msg size
+extern unsigned g_tw_max_events_buffered;         ///< event buffer size
+extern unsigned g_tw_max_remote_events_buffered;  ///< remote event buffer size
+///@}
+
+/** \name Scheduler Configuration */
+///@{
+extern unsigned g_tw_synchronization_protocol;    ///< scheduler type
+extern unsigned g_tw_mblock;                      ///< batch size for event execution
+extern tw_stime g_tw_ts_end;                      ///< end time of simulation
+///@}
+
+/** \name GVT Configuration */
+///@{
+extern unsigned g_tw_gvt_scheme;      ///< GVT algorithm
+extern unsigned g_tw_gvt_interval;    ///< distance between GVT calls
+extern unsigned g_tw_gvt_trigger;     ///< unit of the GVT interval (time or events)
+extern unsigned g_tw_gvt_phases;      ///< number of phases in a phased gvt
+extern unsigned g_tw_gvt_bucket_size; ///< size of each bucket in a bucketed gvt
+extern unsigned g_tw_async_reduction; ///< use asynchronous reductions in GVTs
+///@}
+
+/** \name LB Configuration */
+///@{
+extern unsigned g_tw_ldb_first;     ///< first iteration to call load balancing
+extern unsigned g_tw_ldb_interval;  ///< number of intervals to wait before lb
+extern unsigned g_tw_max_ldb;       ///< max number of times to call lb
+extern unsigned g_tw_ldb_metric;    ///< metric used to measure LP load
+extern unsigned g_tw_metric_ts_abs; ///< use absolute time or not
+extern unsigned g_tw_metric_invert; ///< whether or not to invert metric
+///@}
+
+/** \name RNG Configuration */
+///@{
+extern tw_seed* g_tw_rng_seed;    ///< initial seed for all RNGs
+extern size_t   g_tw_rng_max;     ///< \todo documentation needed
+extern unsigned g_tw_nRNG_per_lp; ///< number of RNGs per LP
+extern unsigned g_tw_rng_default; ///< \todo documentation needed
+///@}
+
+/** \name Misc Configuration */
+///@{
+extern int      g_tw_expected_events; ///< expected number of committed events
+extern unsigned gvt_print_interval;   ///< frequency of progress print outs
+extern unsigned g_tw_stat_interval;   ///< frequency of logging stats (in GVTs)
+///@}
 
 class EventBuffer;
-// A struct for holding global variables used by ROSS. An instance of this
-// struct will be held by each PE group chare.
+
+/**
+ * PE-level variables which may change over time
+ */
 class Globals {
   public:
-    Time g_last_gvt;            // TODO: Needed for rollbacks, but should be moved
-    Time g_leash_time;          // Used for leash trigger
-    // Differs by PE
-    tw_event*     abort_event;  // TODO: Can this just be part of event buffer? Do we need this at all even?
-    EventBuffer*  event_buffer; // TODO: Move to scheduler
-    AvlTree       avl_list_head;      // TODO: Move to optimistic scheduler or maybe even LP, experiment with unordered_map
+    /**
+     * Last computed GVT, used for rollbacks.
+     * \todo This should be moved to GVT base class probably
+     */
+    Time g_last_gvt;
+    /**
+     * Start of the virtual time leash when using virtual time as a GVT trigger.
+     * When a GVT is completed this gets set as that time. During the GVT
+     * computation it is set as the time this PE contributed. The only time this
+     * will actually come into effect is with asynchronous GVTs.
+     */
+    Time g_leash_time;
+    /**
+     * Sentinel event signifying some kind of error.
+     * \todo Is this the best place to store this?
+     */
+    tw_event* abort_event;
+    /**
+     * Buffer of pre-allocated events for entire simulation.
+     * \todo Can probably be moved to scheduler, which will handle event
+     * allocation and management.
+     */
+    EventBuffer* event_buffer;
+    /**
+     * Hash table for events received remotely in optimistic execution.
+     * \todo Can this go in the LP class for smaller hash tables?
+     * \todo Can this be changed to an unordered_map?
+     */
+    AvlTree avl_list_head;
 
     Globals() : g_last_gvt(0.0), g_leash_time(0.0),
         abort_event(NULL), event_buffer(NULL), avl_list_head(NULL) {}
 };
 
-void clear_globals();
-Globals* get_globals();
+void clear_globals();   ///< sets all global variables to default values
+Globals* get_globals(); ///< returns a pointer to the local Globals object
 
+/** Macro to simplify global variable access */
 #define PE_VALUE(x) get_globals()->x
 
 #endif
