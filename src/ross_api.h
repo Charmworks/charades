08c129f07e00107e031f7a241374982030884850
Eric Mikida <mikida2@illinois.edu>
2017-07-11 11:59:25 -0500

    Add documentation to ross_api.h

--- src/ross_api.h
+++ src/ross_api.h
@@ -1,3 +1,10 @@
+/** \file ross_api.h
+ *  Single include for models which includes all other required headers.
+ *
+ *  \todo Get rid of/move the other random declarations and defines as needed.
+ *  \todo Rename file for Charades
+ */
+
 #ifndef _ROSS_API_H
 #define _ROSS_API_H
 
@@ -12,14 +19,16 @@
 #include "util.h"
 
 #include <charm++.h>
+
+/** Macro for determinig the local node number */
 #define g_tw_mynode CkMyPe()
+/** \deprecated Macro for determining the local node size */
 #define g_tw_npe 1
-
-// TODO: Move these
-int tw_ismaster();
-int tw_nnodes();
-tw_stime tw_now(tw_lp*);
-
+/** Macro to access the userData for an event as a void* */
 #define tw_event_data(e) (e->userData)
+
+int tw_ismaster();        ///< returns true if we are rank 0
+int tw_nnodes();          ///< returns the number of nodes in the job
+tw_stime tw_now(tw_lp*);  ///< returns the current time of the given lp
 
 #endif
