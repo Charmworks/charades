7b168e6b42390efd406e5902958d31fe42226053
Eric Mikida <mikida2@illinois.edu>
2017-07-12 16:47:03 -0500

    Add typedef documentation

--- src/typedefs.h
+++ src/typedefs.h
@@ -1,3 +1,16 @@
+/** \file typedefs.h
+ *  Declares most types used within the simulator and by models.
+ *
+ *  Many of these types are just renaming builtin or existing types, or aliasing
+ *  other types. Needs a lot of cleanup. Some types are not used, or are
+ *  misleading since the conversion of ROSS.
+ *
+ *  \todo Cleanup unneeded types
+ *  \todo Move type declarations to appropriate modules as needed
+ *  \todo Maybe make a seperate file for aliases that is only needed when using
+ *  the old ROSS API.
+ */
+
 #ifndef TYPEDEFS_H_
 #define TYPEDEFS_H_
 
@@ -5,13 +18,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-// TODO: Make this a more complete listing of types?
-// TODO: Should we have a central file for all types, or just define types
-// where they are needed.
-
-/**
- * Synchronization protocol used
- */
+/** Enum for synchroniaztion protocol. \todo move to scheduler */
 enum tw_synch_e {
     NO_SYNCH,
     SEQUENTIAL,
@@ -19,69 +26,90 @@ enum tw_synch_e {
     OPTIMISTIC,
 };
 
+/** Struct for recording memory usage. \todo is this even used? */
 struct MemUsage {
   unsigned long long max_memory;
   double avg_memory;
 };
 
-// Types used in the Charm++ backend of the framework
-typedef double Time;
+/** \name Original ROSS typedefs
+ *  These are typedefs from original ROSS.
+ *  \todo some are not needed anymore since they just typedef structs
+ *////@{
+typedef unsigned long long EventID; ///< Unique ID for events sent from one LP
+typedef unsigned long long tw_lpid; ///< LP ID, either global or local
+typedef unsigned long long tw_peid; ///< \deprecated PE ID
+typedef unsigned long long tw_stat; ///< Typedef for integer stats \todo not needed
+typedef uint64_t tw_clock;          ///< \deprecated Clock value? \todo this is not used anywhere
+typedef struct avlNode *AvlTree;    ///< Node in the PE level AVL tree
+typedef struct tw_bf tw_bf;         ///< Bitfield for reverse computation \todo typedef unneeded.
+typedef int32_t* tw_seed;           ///< Type of the RNG seed use by all RNGs
+///@}
 
+/** \name Charm++ Types/Forward Declarations
+ *  Most of these types were introduced for the Charm++ side, and have aliases
+ *  to keep compatible with old ROSS.
+ *  \see LPStruct
+ *  \see LPType
+ *  \see LP
+ *  \see Event
+ *  \see ProcessedQueue
+ *  \see PendingQueue
+ *  \todo Many of these forward declarations may not even be needed
+ *////@{
+/**
+ * Typedef for virtual time type, aliased to tw_stime as well
+ * \todo Can time be model defined? How easy is it to switch to an int/long?
+ * */
+typedef double Time;
 class LPStruct;
 class LPType;
 class LP;
 class Event;
 class ProcessedQueue;
 class PendingQueue;
+///@}
 
+/** \name Charm++ Aliases
+ *  This small API aliases our Charm++ types to old ROSS types
+ *  \todo Get rid of or move to separate place for old ROSS models
+ *////@{
+typedef Time tw_stime;      ///< Alias for virtual time type
+typedef EventID tw_eventid; ///< \deprecated Alias for event IDs
+typedef Event tw_event;     ///< Alies for event structure
+typedef LPStruct tw_lp;     ///< Alias for the model defined LP objects
+typedef LPType tw_lptype;   ///< Alias for the LP type structure
+typedef LP tw_pe;           ///< \deprecated Treats LP chares as old ROSS pes
+///@}
 
-typedef unsigned long long EventID;
-//typedef enum tw_event_owner tw_event_owner;
-typedef unsigned long long tw_lpid;
-typedef unsigned long long tw_peid;
-typedef struct avlNode *AvlTree;
-typedef struct tw_bf tw_bf;
-
-// API layer linking our types to ROSS types
-typedef Time tw_stime;
-typedef EventID tw_eventid;
-typedef Event tw_event;
-typedef LPStruct tw_lp;
-typedef LPType tw_lptype;
-
-// Statistics
-typedef unsigned long long tw_stat;
-typedef uint64_t tw_clock;
-
-// TODO: This doesn't really make sense;
-typedef LP tw_pe;
-
-// These typedefs are similar to function ptr typedefs used by ROSS.
-// TODO: Need to figure out how we are doing maps exactly, and how many we need.
-// TODO: How much do we need this API to match directly to the ROSS equivalent?
+/** \name Function Pointers
+ *  These are typedefs for functions that make up LPType and map LPs.
+ *////@{
+/** Called at simulation start for LP initialization */
 typedef void (*init_f) (void*,LPStruct*);
+/** Forward event handler */
 typedef void (*event_f) (void*, tw_bf*, char*, LPStruct*);
+/** Reverse event handler */
 typedef void (*revent_f) (void*, tw_bf*, char*, LPStruct*);
-typedef void (*final_f) (void*, LPStruct*);
+/** Commit event handler */
 typedef void (*commit_f) (void*, tw_bf*, char*, LPStruct*);
+/** Called at simulation end for finalization */
+typedef void (*final_f) (void*, LPStruct*);
 
-// This map takes an LP chare index, and a local lpid and returns a global id.
+/** Map that takes an LP chare index and local LP id and returns a global id */
 typedef tw_lpid (*init_map_f) (unsigned, tw_lpid);
-// This map takes a global lpid and returns the chare index containing that lp.
+/** Map that takes a global LP id and returns the chare where that LP resides */
 typedef unsigned (*chare_map_f) (tw_lpid);
-// This map takes an LP global id, and returns the local id.
+/** Map that takes a global LP id and returns a local id within its chare */
 typedef tw_lpid (*local_map_f) (tw_lpid);
-// This map takes an LP chare index and returns how many LPs are on that chare.
+/** Map that takes a chare index and returns the number of LPs on that chare */
 typedef unsigned (*numlp_map_f) (unsigned);
 
-// This map takes a global lpid and returns the type of that lp.
-// Will be defined in the model.
+/** Map that takes a global LP id and returns a pointer to the LP type */
 typedef LPType* (*type_map_f) (tw_lpid);
+///@}
 
-// Typedefs for rand
-typedef int32_t* tw_seed;
-
-// TODO: This should be somewhere else
+/** Macro for debug printing \todo should be removed or moved elsewhere */
 #define TW_LOC  __FILE__,__LINE__
 
 #endif
