e734a2c89663f1ceb8cc38885f621b057acde1e1
Eric Mikida <mikida2@illinois.edu>
2017-08-16 03:00:27 -0500

    Update the example model

--- models/example/example.C
+++ models/example/example.C
@@ -11,115 +11,130 @@ Time g_initial_events;  ///< Number of events sent out during initialization
 Time g_mean_delay;      ///< Mean delay in virtual time of new events
 Time g_adjusted_mean;   ///< Mean value for RNG, adjusted for lookahead
 
-void initialize(ExampleState* state, tw_lp* lp) {
-  /** Set our LPs counter to 0, and compute the neighboring global ID */
-  state->counter = 0;
-  state->neighbor = (lp->gid + 1) % g_total_lps;
+void ExampleLP::initialize() {
+  counter = 0;
+  neighbor = (gid + 1) % g_total_lps;
 
-  /**
-   * Send out g_initial_events events to start the simulation, all to our
-   * neighoring LP, with payload equal to our counter, which is incremented with
-   * each event sent.
-   */
   for (int i = 0; i < g_initial_events; i++) {
-    /**
-     * Use tw_rand_exponential to generate a random double. To get deterministic
-     * simulations, tw_rand_reverse_unif() needs to be called in the reverse
-     * handler to reset the RNG to its earlier state.
-     */
-    Time delay = g_tw_lookahead + tw_rand_exponential(lp->rng, g_adjusted_mean);
-    /**
-     * Create a new event for our neighbor LP, at our current virtual time plus
-     * delay, and with lp as the event source.
-     */
-    Event* e = tw_event_new(state->neighbor, delay, lp);
-    /**
-     * Set the message specific data, which for this model involves incrementing
-     * the counter in state. This means in the reverse handler, the counter will
-     * need to be decremented.
-     */
-    ExampleMessage* msg = (ExampleMessage*)tw_event_data(e);
-    msg->payload = state->counter++;
+    Time delay = g_tw_lookahead + g_adjusted_mean*tw_rand_exponential(rng, 1.0);
+    // tw_event_new now takes a template parameter for message type. It creates
+    // an event with that message type, and calls the default constructor of
+    // that message type to initialize the events memory.
+    Event* e = tw_event_new<ExampleMessage>(gid, delay, this);
+    // get_data returns a pointer to the message data, already cast to the
+    // correct type. In the future, this will also include optional error
+    // checking to make sure the cast is legal.
+    ExampleMessage* msg = e->get_data<ExampleMessage>();
+    msg->payload = counter++;
     tw_event_send(e);
   }
 }
+void ExampleLP::finalize() {}
 
-void forward(ExampleState* state, tw_bf* bf, ExampleMessage* msg, tw_lp* lp) {
-  /**
-   * To forward execute an event, we just create a new event and set its
-   * payload to be the sum of our current counter and the payload of the
-   * incoming event. All other API calls are the same as in initialize().
-   */
-  Time delay = g_tw_lookahead + tw_rand_exponential(lp->rng, g_adjusted_mean);
-  Event* e = tw_event_new(state->neighbor, delay, lp);
-  ExampleMessage* new_msg = (ExampleMessage*)tw_event_data(e);
-  new_msg->payload = msg->payload + state->counter++;
+void ExampleLP::forward(ExampleMessage* msg, tw_bf* bf) {
+  Time delay = g_tw_lookahead + g_adjusted_mean*tw_rand_exponential(rng, 1.0);
+  Event* e = tw_event_new<ExampleMessage2>(neighbor, delay, this);
+  ExampleMessage2* new_msg = e->get_data<ExampleMessage2>();
+  new_msg->payload = msg->payload + counter++;
   tw_event_send(e);
 }
 
-void reverse(ExampleState* state, tw_bf* bf, ExampleMessage* msg, tw_lp* lp) {
-  /**
-   * To reverse an event we need to undo all of its changes on the state of the
-   * simulation, including the state of the random number generators. In this
-   * case it is just reversing a single RNG call, and decrementing the counter.
-   */
-  tw_rand_reverse_unif(lp->rng);
-  state->counter--;
+void ExampleLP::reverse(ExampleMessage* msg, tw_bf* bf) {
+  tw_rand_reverse_unif(rng);
+  counter--;
 }
 
-void commit(ExampleState* state, tw_bf* bf, ExampleMessage* msg, tw_lp* lp) {
-  /**
-   * The events in this model don't have any side-effects that can't be
-   * reversed, so there is no need to do anything at commit time.
-   */
+void ExampleLP::commit(ExampleMessage* msg, tw_bf* bf) {}
+
+void ExampleLP::forward(ExampleMessage2* msg, tw_bf* bf) {
+  Time delay = g_tw_lookahead + g_adjusted_mean*tw_rand_exponential(rng, 1.0);
+  Event* e = tw_event_new<ExampleMessage>(neighbor, delay, this);
+  ExampleMessage* new_msg = e->get_data<ExampleMessage>();
+  new_msg->payload = msg->payload + counter++;
+  tw_event_send(e);
+}
+void ExampleLP::reverse(ExampleMessage2* msg, tw_bf* bf) {
+  tw_rand_reverse_unif(rng);
+  counter--;
 }
 
-void finalize(ExampleState* state, tw_lp* lp) {
-  /** The LPs in this model don't need to do anything at finalization time */
+void ExampleLP::commit(ExampleMessage2* msg, tw_bf* bf) {}
+
+void ExampleLP2::initialize() {
+  counter = 0;
+  neighbor = (gid + 1) % g_total_lps;
+
+  for (int i = 0; i < g_initial_events; i++) {
+    Time delay = g_tw_lookahead + g_adjusted_mean*tw_rand_exponential(rng, 1.0);
+    Event* e = tw_event_new<ExampleMessage2>(gid, delay, this);
+    ExampleMessage2* msg = e->get_data<ExampleMessage2>();
+    msg->payload = counter++;
+    tw_event_send(e);
+  }
+}
+void ExampleLP2::finalize() {}
+
+void ExampleLP2::forward(ExampleMessage* msg, tw_bf* bf) {
+  Time delay = g_tw_lookahead + g_adjusted_mean*tw_rand_exponential(rng, 1.0);
+  Event* e = tw_event_new<ExampleMessage2>(neighbor, delay, this);
+  ExampleMessage2* new_msg = e->get_data<ExampleMessage2>();
+  new_msg->payload = msg->payload + counter++;
+  tw_event_send(e);
 }
 
-/**
- * We can define as many LPTypes as we like, which consist of a set of handlers
- * and a state size. During initialization of the simulation, a type mapping
- * will be used to assign types to LPs based on their global IDs.
- */
-LPType example_type = {
-  (init_f) initialize,
-  (event_f) forward,
-  (revent_f) reverse,
-  (commit_f) commit,
-  (final_f) finalize,
-  sizeof(ExampleState)
-};
-
-/**
- * A mapping of global ID to LPType. In this simple example, all LPs have the
- * same type.
- */
-LPType* example_type_map(tw_lpid gid) {
-  return &example_type;
+void ExampleLP2::reverse(ExampleMessage* msg, tw_bf* bf) {
+  tw_rand_reverse_unif(rng);
+  counter--;
 }
 
-/**
- * The main function is responsible for setting up the simulation. This entails
- * defining what an LP is, how many LPs there are, and how they map to chares.
- * This is also where the scheduler and GVT algorithms can be configured, before
- * making a call to tw_run() to actually start the simulation.
- */
+void ExampleLP2::commit(ExampleMessage* msg, tw_bf* bf) {}
+
+void ExampleLP2::forward(ExampleMessage2* msg, tw_bf* bf) {
+  Time delay = g_tw_lookahead + g_adjusted_mean*tw_rand_exponential(rng, 1.0);
+  Event* e = tw_event_new<ExampleMessage>(neighbor, delay, this);
+  ExampleMessage* new_msg = e->get_data<ExampleMessage>();
+  new_msg->payload = msg->payload + counter++;
+  tw_event_send(e);
+}
+
+void ExampleLP2::reverse(ExampleMessage2* msg, tw_bf* bf) {
+  tw_rand_reverse_unif(rng);
+  counter--;
+}
+
+void ExampleLP2::commit(ExampleMessage2* msg, tw_bf* bf) {}
+
+/** LP Factory */
+LPBase* example_type_map(uint64_t gid) {
+  if (gid % 2 == 0) {
+    return new ExampleLP();
+  } else {
+    return new ExampleLP2();
+  }
+}
+
 int main(int argc, char** argv) {
   tw_init(argc, argv);
 
-  g_tw_lookahead = 0.1;
-  g_tw_ts_end = 1024.0;
+  // All message types must be registered at the beginning to set up the
+  // message dispatch framework.
+  register_msg_type<ExampleMessage>();
+  register_msg_type<ExampleMessage2>();
+
+  g_tw_lookahead = 100;
+  g_tw_ts_end = 128000;
+  g_tw_expected_events = 8100;
   g_total_lps = 16;
   g_lps_per_chare = 1;
 
+
   g_initial_events = 4;
-  g_mean_delay = 1.0;
+  g_mean_delay = 1000;
   g_adjusted_mean = g_mean_delay - g_tw_lookahead;
 
   g_type_map = example_type_map;
-  tw_define_lps(sizeof(ExampleMessage), 0);
+
+  tw_create_lps();
   tw_run();
   tw_end();
 
--- models/example/example.h
+++ models/example/example.h
@@ -1,104 +1,56 @@
-/**
- * \file example.h
- * Struct declarations used by the model. Specifically, here is where we would
- * usually define the LP state structs and event message structs. Also may
- * include global variable decls and/or method declarations.
- */
-
 #ifndef EXAMPLE_H_
 #define EXAMPLE_H_
 
-#include "ross_api.h"
+#include "charades.h"
 
-/**
- * This is the type of state kept for each LP. In this simple example, each LP
- * stores the ID of its neighboring LP and a counter of events it has sent.
- */
-struct ExampleState {
-  int neighbor; ///< ID of the neighboring LP
-  int counter;  ///< Number of events that have been sent
-};
-
-/**
- * This is the type of the model specific data sent with each event. In this
- * simple example, this is just a single number. The payload of each event is
- * calculated as the sum of the received event that caused the new event to be
- * sent, and the current counter value of the receiving LP.
- */
+// Declare message types as structs or classes. No inheritance needed.
 struct ExampleMessage {
-  int payload;  ///< Payload of the event
+  int payload;
 };
 
-/**
- * \name LP Handlers
- *
- * To define an LP, you need 5 different handlers: initialize, forward, reverse,
- * commit, and finalize. Not all of these handlers need to have non-empty bodies
- * depending on the functionality your simulation requires.
- *
- * Initialize and forward always need code, and make up the core of an LP
- * defintion:
- * - Intialize is called before the simulation begins, and is used to
- *   set up the initial LP state and send out initial messages. Without initial
- *   messages the simulation wouldn't have anything to run and would immediately
- *   end.
- * - Forward is called to handle every event sent to the passed in LP.
- *
- * Reverse and commit handlers are only used if you need to run your simulation
- * with optimistic synchronization.
- * - Reverse is required, and called to reverse the effects of the passed in
- *   event on the passed in LP. In order to do so, it is often the case that
- *   the bitfield is set in forward save information about branches and loops.
- * - Commit is optional, and is called after GVT on each event that is ready to
- *   be committed. Any non-reverisble side effects such as I/O should be done
- *   in this function. If there are no such side-effects, the function can still
- *   be left empty.
- *
- * The finalize handler is called after the simulation, and can be used for
- * things like statistics collection and logging, but can be left empty if
- * needed.
- *////@{
+struct ExampleMessage2 {
+  int payload;
+};
 
-/**
- * Set up the initial state and send out initial events.
- * \param [out] state a pointer to the LPs state
- * \param [in]  lp a pointer to the LPStruct this handler is being called for
- */
-void initialize(ExampleState* state, tw_lp* lp);
+// Declare LPs with public inheritance from LP<...>
+// The first template argument is the derived type, for CRTP
+// All subsequent types are the types of messages this LP will have to respond
+// to. The base class will automatically register message dispatchers for each
+// template argument. Alternatively, if the template arguments are left out
+// registration can be done manually by calling "register_msg_type<Type>() from
+// within your constructor.
+class ExampleLP : public LP<ExampleLP, ExampleMessage, ExampleMessage2> {
+  private:
+    int neighbor, counter;
+  public:
+    ExampleLP() {}
+    void initialize();
+    void finalize();
 
-/**
- * Execute an event by updating the state and bitfield based on the message.
- * \param [in,out]  state a pointer to the LPs state
- * \param [out]     bf the bitfield correspdonding to this message
- * \param [in]      msg the message data for the event being executed
- * \param [in]      lp a pointer to the LPStruct the event was sent to
- */
-void forward(ExampleState* state, tw_bf* bf, ExampleMessage* msg, tw_lp* lp);
+    void forward(ExampleMessage* msg, tw_bf* bf);
+    void reverse(ExampleMessage* msg, tw_bf* bf);
+    void commit(ExampleMessage* msg, tw_bf* bf);
 
-/**
- * Reverse an event by undoing all of its effects on the state and the RNG.
- * \param [in,out]  state a pointer to the LPs state
- * \param [in]      bf the bitfield correspdonding to this message
- * \param [in]      msg the message data for the event being executed
- * \param [in]      lp a pointer to the LPStruct the event was sent to
- */
-void reverse(ExampleState* state, tw_bf* bf, ExampleMessage* msg, tw_lp* lp);
+    void forward(ExampleMessage2* msg, tw_bf* bf);
+    void reverse(ExampleMessage2* msg, tw_bf* bf);
+    void commit(ExampleMessage2* msg, tw_bf* bf);
+};
 
-/**
- * Commit any side effects of the event that can't be speculative.
- * \param [in,out]  state a pointer to the LPs state
- * \param [in]      bf the bitfield correspdonding to this message
- * \param [in]      msg the message data for the event being executed
- * \param [in]      lp a pointer to the LPStruct the event was sent to
- */
-void commit(ExampleState* state, tw_bf* bf, ExampleMessage* msg, tw_lp* lp);
+class ExampleLP2 : public LP<ExampleLP2, ExampleMessage, ExampleMessage2> {
+  private:
+    int neighbor, counter;
+  public:
+    ExampleLP2() {}
+    void initialize();
+    void finalize();
 
-/**
- * Do any post processing after simulation, such as logging or stat collection.
- * \param state a pointer to the LPs state
- * \param lp a pointer to the LPStruct this handler is being called for
- */
-void finalize(ExampleState* state, tw_lp* lp);
-///@}
+    void forward(ExampleMessage* msg, tw_bf* bf);
+    void reverse(ExampleMessage* msg, tw_bf* bf);
+    void commit(ExampleMessage* msg, tw_bf* bf);
+
+    void forward(ExampleMessage2* msg, tw_bf* bf);
+    void reverse(ExampleMessage2* msg, tw_bf* bf);
+    void commit(ExampleMessage2* msg, tw_bf* bf);
+};
 
 #endif
